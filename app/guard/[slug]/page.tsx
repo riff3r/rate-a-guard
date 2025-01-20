4daacee4786cecbe0488d849ae93654f23a676dc
riff3r <arif4alll@gmail.com>
2025-01-20 19:17:55 +0600

    minor change

--- app/guard/[slug]/page.tsx
+++ app/guard/[slug]/page.tsx
@@ -1,12 +1,14 @@
+import GuardRatingChart from "@/components/guard/GuardRatingChart";
 import { Button } from "@/components/ui/button";
-import { ArrowRight, Bookmark } from "lucide-react";
+import { ArrowRight, Bookmark, ChevronDown } from "lucide-react";
 import React from "react";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const page = () => {
   return (
     <div>
-      <div className="flex">
-        <div className="w-1/2">
+      <div className="flex gap-10">
+        <div className="w-4/12">
           <div className="flex gap-2 mb-2">
             <div className="text-7xl font-poppins  font-[900] ">4.6</div>
             <div className="text-base text-gray-400 font-bold top-3 relative">
@@ -33,7 +35,7 @@ const page = () => {
               Rate <ArrowRight strokeWidth={3} />
             </Button>
 
-            <div className="font-bold mb-5">Professor Moraguez's Top Tags</div>
+            <div className="font-bold mb-5">Professor Moraguezs Top Tags</div>
 
             <div className="flex gap-2">
               <Button
@@ -53,8 +55,53 @@ const page = () => {
             </div>
           </div>
         </div>
-        <div>
-            
+
+        <div className="w-4/12">
+          <GuardRatingChart />
+
+          <div className="p-5 bg-primary-foreground">
+            <p className="font-bold mb-5">Similar Professors</p>
+
+            <div className="flex gap-2">
+              <div className="flex items-center gap-2 w-full">
+                <div className="bg-primary font-poppins text-white font-bold py-2 px-4 flex justify-center items-center">
+                  4.80
+                </div>
+
+                <div>Lor aine Smith</div>
+              </div>
+              <div className="flex items-center gap-2 w-full">
+                <div className="bg-primary font-poppins text-white font-bold py-2 px-4 flex justify-center items-center">
+                  4.80
+                </div>
+
+                <div>John Doe</div>
+              </div>
+              <div className="flex items-center gap-2 w-full">
+                <div className="bg-primary font-poppins text-white font-bold py-2 px-4 flex justify-center items-center">
+                  4.80
+                </div>
+
+                <div>Aine Arsmith</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-8/12">
+        <Tabs defaultValue="profile">
+          <TabsList className="mb-5">
+            <TabsTrigger value="profile">22 Student Ratings</TabsTrigger>
+          </TabsList>
+        </Tabs>
+
+        <Button variant="outline" className="font-bold mb-5">
+          All <ChevronDown strokeWidth={3} />
+        </Button>
+
+        <div className="flex flex-col gap-5">
+          <div className="flex gap-2 p-5 bg-primary-foreground"></div>
         </div>
       </div>
     </div>
--- components/dashboard/navbar/Navbar.tsx
+++ components/dashboard/navbar/Navbar.tsx
@@ -1,102 +1,138 @@
 "use client";
-import React, { useState } from 'react';
-import { Building2, ChevronDown, Pencil, User } from 'lucide-react';
-import { Input } from '../../ui/input';
-import { Button } from '../../ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../../ui/dropdown-menu';
-import { PopoverTrigger } from '@radix-ui/react-popover';
-import { Popover, PopoverContent } from '../../ui/popover';
+import React, { useState } from "react";
+import { Building2, ChevronDown, Pencil, User } from "lucide-react";
+import { Input } from "../../ui/input";
+import { Button } from "../../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../../ui/dropdown-menu";
+import { PopoverTrigger } from "@radix-ui/react-popover";
+import { Popover, PopoverContent } from "../../ui/popover";
 
 const Navbar: React.FC = () => {
-    const [isSearchingGuard, setIsSearchingGuard] = useState(true);
-    const [isSearchingCompany, setIsSearchingCompany] = useState(false);
-    return (
-        <div className='bg-foreground mb-8 px-6 py-3'>
-            <div className='container mx-auto'>
-                <div className='flex items-center justify-between'>
-                    <div className='flex items-center gap-6'>
-                        <div className='logo bg-white px-6 py-1 text-center text-lg font-semibold text-black'>RAG</div>
-
-                        <DropdownMenu>
-                            <DropdownMenuTrigger className='flex items-center text-white'>
-                                {isSearchingGuard ? <User size={20} /> : <Building2 size={20} />}{' '}
-                                <div className='ml-1 mr-2'>{isSearchingGuard ? 'Guard' : 'Company'}</div>
-                                <ChevronDown size={20} strokeWidth={3} />
-                            </DropdownMenuTrigger>
-                            
-                            <DropdownMenuContent onClick={() => setIsSearchingGuard(!isSearchingGuard)}>
-                                {isSearchingGuard ? (
-                                    <DropdownMenuItem>
-                                        <Building2 size={20} />
-                                        Company
-                                    </DropdownMenuItem>
-                                ) : (
-                                    <DropdownMenuItem>
-                                        <User size={20} />
-                                        Guard
-                                    </DropdownMenuItem>
-                                )}
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                        
-                        <Popover>
-                            <PopoverTrigger><Input type='text' placeholder={isSearchingGuard ? 'Guard Name' : 'Company Name'} className='w-[350px]' /></PopoverTrigger>
-                            <PopoverContent>Place content for the popover here.</PopoverContent>
-                        </Popover>
-
-                        {isSearchingGuard && (
-                            <>
-                                <div className='text-white'>at</div>
-                                {isSearchingCompany ? (
-                                    <Input
-                                        type='text'
-                                        placeholder='Company Name'
-                                        className='w-[250px]'
-                                        onMouseLeave={() => setIsSearchingCompany(false)}
-                                    />
-                                ) : (
-                                    <div className='flex items-center gap-4'>
-                                        <div
-                                            className='underline-offset-3 cursor-pointer font-bold text-white underline'
-                                            onClick={() => setIsSearchingCompany(true)}
-                                        >
-                                            Guard Company
-                                        </div>
-                                        <Pencil
-                                            color='white'
-                                            size={18}
-                                            className='cursor-pointer'
-                                            onClick={() => setIsSearchingCompany(true)}
-                                        />
-                                    </div>
-                                )}
-                            </>
-                        )}
-                    </div>
-
-                    <div className='flex items-center gap-4'>
-                        <DropdownMenu>
-                            <DropdownMenuTrigger className='flex items-center text-white'>
-                                <Button size='sm' className='rounded-full font-semibold bg-foreground hover:bg-neutral-700 focus: outline-none'>
-                                    Hey, User
-                                </Button>
-                            </DropdownMenuTrigger>
-
-                            <DropdownMenuContent align='end'>
-                                <DropdownMenuItem className='focus:bg-primary focus:text-white'>Profile</DropdownMenuItem>
-                                <DropdownMenuItem className='focus:bg-primary focus:text-white'>Account Settings</DropdownMenuItem>
-                                <DropdownMenuItem className='focus:bg-primary focus:text-white'>Your Ratings</DropdownMenuItem>
-                                <DropdownMenuItem className='focus:bg-primary focus:text-white'>Saved Guards</DropdownMenuItem>
-                                <DropdownMenuItem className='focus:bg-primary focus:text-white'>Logout</DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-
-                        {/* <Button className='rounded-full bg-white font-semibold text-black hover:bg-neutral-700 hover:text-white h-8'>Help</Button> */}
-                    </div>
-                </div>
+  const [isSearchingGuard, setIsSearchingGuard] = useState(true);
+  const [isSearchingCompany, setIsSearchingCompany] = useState(false);
+  return (
+    <div className="bg-foreground px-6 py-3">
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-6">
+            <div className="logo bg-white px-6 py-1 text-center text-lg font-semibold text-black">
+              RAG
             </div>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger className="flex items-center text-white">
+                {isSearchingGuard ? (
+                  <User size={20} />
+                ) : (
+                  <Building2 size={20} />
+                )}{" "}
+                <div className="ml-1 mr-2">
+                  {isSearchingGuard ? "Guard" : "Company"}
+                </div>
+                <ChevronDown size={20} strokeWidth={3} />
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent
+                onClick={() => setIsSearchingGuard(!isSearchingGuard)}
+              >
+                {isSearchingGuard ? (
+                  <DropdownMenuItem>
+                    <Building2 size={20} />
+                    Company
+                  </DropdownMenuItem>
+                ) : (
+                  <DropdownMenuItem>
+                    <User size={20} />
+                    Guard
+                  </DropdownMenuItem>
+                )}
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            <Popover>
+              <PopoverTrigger>
+                <Input
+                  type="text"
+                  placeholder={isSearchingGuard ? "Guard Name" : "Company Name"}
+                  className="w-[350px]"
+                />
+              </PopoverTrigger>
+              <PopoverContent>
+                Place content for the popover here.
+              </PopoverContent>
+            </Popover>
+
+            {isSearchingGuard && (
+              <>
+                <div className="text-white">at</div>
+                {isSearchingCompany ? (
+                  <Input
+                    type="text"
+                    placeholder="Company Name"
+                    className="w-[250px]"
+                    onMouseLeave={() => setIsSearchingCompany(false)}
+                  />
+                ) : (
+                  <div className="flex items-center gap-4">
+                    <div
+                      className="underline-offset-3 cursor-pointer font-bold text-white underline"
+                      onClick={() => setIsSearchingCompany(true)}
+                    >
+                      Guard Company
+                    </div>
+                    <Pencil
+                      color="white"
+                      size={18}
+                      className="cursor-pointer"
+                      onClick={() => setIsSearchingCompany(true)}
+                    />
+                  </div>
+                )}
+              </>
+            )}
+          </div>
+
+          <div className="flex items-center gap-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger className="flex items-center text-white">
+                <Button
+                  size="sm"
+                  className="rounded-full font-semibold bg-foreground hover:bg-neutral-700 focus: outline-none"
+                >
+                  Hey, User
+                </Button>
+              </DropdownMenuTrigger>
+
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem className="focus:bg-primary focus:text-white">
+                  Profile
+                </DropdownMenuItem>
+                <DropdownMenuItem className="focus:bg-primary focus:text-white">
+                  Account Settings
+                </DropdownMenuItem>
+                <DropdownMenuItem className="focus:bg-primary focus:text-white">
+                  Your Ratings
+                </DropdownMenuItem>
+                <DropdownMenuItem className="focus:bg-primary focus:text-white">
+                  Saved Guards
+                </DropdownMenuItem>
+                <DropdownMenuItem className="focus:bg-primary focus:text-white">
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            {/* <Button className='rounded-full bg-white font-semibold text-black hover:bg-neutral-700 hover:text-white h-8'>Help</Button> */}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
