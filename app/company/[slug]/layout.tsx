06607ce9955019a7d7c8f25c56a6ec78d261b998
Raihanul Islam <raihanul@orangetoolz.com>
2025-02-06 12:58:20 +0600

    design fix

--- app/company/[slug]/layout.tsx
+++ app/company/[slug]/layout.tsx
@@ -11,7 +11,7 @@ const RootLayout = async ({
         <>
             <Navbar />
 
-            <div className="h-[calc(100vh-136px)] overflow-y-auto">
+            <div className="h-[calc(100vh-140px)] overflow-y-auto">
                 <div className="mx-auto max-w-[1240px] py-10">{children}</div>
             </div>
 
